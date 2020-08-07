23efdafcde1825d121d2edaa04fb7f2d7a326917
ShaWn <smorrison1986@gmail.com>
2020-08-06 23:17:41 -0500

    Configuring mDNS.

--- PrivateHome/Program.cs
+++ PrivateHome/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Threading.Tasks;
+using Makaretu.Dns;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +15,48 @@ namespace PrivateHome
     {
         public static void Main(string[] args)
         {
+            var serviceName = "privatehome.local";
+            var mdns = new MulticastService();
+            mdns.QueryReceived += (s, e) =>
+            {
+                var msg = e.Message;
+                var question = msg.Questions.FirstOrDefault();
+                var isEqual = question.Name == serviceName;
+                if (msg.Questions.Any(q => q.Name == serviceName))
+                {
+                    Console.WriteLine("Received local host lookup question.");
+
+                    var res = msg.CreateResponse();
+                    var allAddresses = MulticastService.GetIPAddresses();
+                    var ipv4Addresses = allAddresses
+                        .Where(ip => ip.AddressFamily == AddressFamily.InterNetwork
+                        && ip.ToString().StartsWith("192."));
+
+                    foreach (var address in ipv4Addresses)
+                    {
+                        res.Answers.Add(new ARecord
+                        {
+                            Name = serviceName,
+                            Address = address
+                        });
+                    }
+
+                    var ipv6Addresses = allAddresses
+                        .Where(ip => ip.AddressFamily == AddressFamily.InterNetworkV6
+                        && ip.ToString().StartsWith("fe80::"));
+                    foreach (var address in ipv6Addresses)
+                    {
+                        res.Answers.Add(new AAAARecord
+                        {
+                            Name = serviceName,
+                            Address = address
+                        });
+                    }
+
+                    mdns.SendAnswer(res);
+                }
+            };
+            mdns.Start();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +65,7 @@ namespace PrivateHome
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80", "https://*:443");
                 });
     }
 }
